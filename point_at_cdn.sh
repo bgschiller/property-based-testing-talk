c5e3c912b93ad3de1b5282ae77d9cc3096467bf2
Brian Schiller <bgschiller@gmail.com>
2017-08-25 21:22:53 -0600

    proxy to cloudflare, re-run cells

--- point_at_cdn.sh
+++ point_at_cdn.sh
@@ -3,5 +3,5 @@
 if [ $# != 2 ]; then
     echo "Usage: [input html] [output html]";
 fi
-sed 's:reveal.js/://cdn.jsdelivr.net/reveal.js/3.5.0/:g' $1 > $2
+sed 's:reveal.js/://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.5.0/:g' $1 > $2
 
